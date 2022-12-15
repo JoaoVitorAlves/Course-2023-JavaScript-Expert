0080e293e954523f668e4aa3a97ade099da4d920
João Vitor Alves de Oliveira <jv.joaovitor.oliveira@gmail.com>
2022-12-15 17:32:29 -0300

    Add files via upload

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,109 @@
+/*let country = 'Portugal';
+let continet = 'Europe';
+let population = 10;
+
+console.log(country);
+console.log(continet);
+console.log(population);
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+
+let javascriptIsfun = true;
+console.log(javascriptIsfun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsfun);
+//console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+//birthYear = 1990;
+
+const job;
+
+
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 2, 2 ** 3);
+//2 ** 3 mean 2 to the power of 3 = 2*2*2
+
+const firsName = 'Jonas';
+const lastName = 'Schmedtman';
+
+console.log(firsName + ' ' + lastName);
+
+// Assigment operators
+let x = 10 + 5;
+x += 10; //x = x + 10 = 25
+x *= 4; //x = x * 4 = 100
+x++; //x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+// > , < , >=, <= , ===, ||
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log((now - 1991) > now - 2018);
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+console.log(25 - 10 - 5);
+
+let x, y;
+
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+*/
+
+// const massMark = 78;
+// const massJhon = 92;
+// const heightMark = 1.69;
+// const heightJhon = 1.95;
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJhon = 85;
+// const heightJhon = 1.76;
+
+// const BMIMark = massMark / heightMark ** 2;
+
+// const BMIJhon = massJhon / (heightJhon * heightJhon);
+
+// const markHigherBMI = BMIMark > BMIJhon;
+
+// const mark = `Mark possui imc ${BMIMark} maior que de Jhon ${BMIJhon}`
+
+
+// console.log(BMIMark, BMIJhon, markHigherBMI);
+
+
+
